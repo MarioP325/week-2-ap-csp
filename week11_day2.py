41b19c4438715258489b5ef68527b374c8eaeb4c
MarioP325 <mapantoja1@cps.edu>
2025-11-03 15:00:30 -0600

    Update week11_day2.py

--- week11_day2.py
+++ week11_day2.py
@@ -9,10 +9,10 @@ print("You’ll review variables, strings, numbers, and print formatting.\n")
 
 # Step 1: Create Variables
 # TODO: Replace the values below with your own info
-first_name = "Marvin"
-age = 30
+first_name = "Mario"
+age = 16
 favorite_color = "Blue"
-favorite_number = 7
+favorite_number = 8
 
 #  Step 2: Practice String Operations
 # 1. Print your name in uppercase
@@ -22,17 +22,24 @@ print(first_name.upper())
 print(len(first_name))
 
 # 3. Combine your name and favorite color into one message
-
+print(first_name + favorite_color)
 
 #  Step 3: Math Practice
 # Use arithmetic operators with your favorite number
-
-
+num0=16
+num1=10
+print(num0 + num1)
 #  Step 4: User Input Practice
 # Ask the user two questions and combine answers
-
+question=input("What number are you thinking of")
+question1=input("what color do you like")
+print("You are thinking of the number", question + " and you like the color", question1)
 
 
 
 # ⚙️ Step 5: Final Challenge (combine it all)
 # Use math and strings together
+print("In 10 years you will be", num1 + num0)
+num3= int(input("number 3 please"))
+num4= int(input("number 4 please"))
+print(num3+num4)

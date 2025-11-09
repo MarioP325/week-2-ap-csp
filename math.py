c8961fa6d8a99685d0030500110eedabcd9eaff3
MarioP325 <mapantoja1@cps.edu>
2025-11-09 01:30:27 +0000

    extra

---
+++ math.py
@@ -0,0 +1,20 @@
+x=3
+y=5
+temp=x
+x=y
+y=temp
+print(x,y)
+print(x+y)
+yourlist=(20,40,60,80)
+mylist=(10,30,50,70)
+yourlist=mylist
+print(yourlist)
+print(f"you have ", x, "dollars with the current rate that it will be", x*y, "dollars")
+a=True
+b=False
+c=True
+a=not a or b and c
+c= c and a
+print(a)
+print(b)
+print(c)
